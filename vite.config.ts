d2bc2da17a591b930ff1d7db5568b09c5f0d2ec7
Glauber Magalhães <29615342+glaubermag@users.noreply.github.com>
2025-06-30 20:23:11 -0300

    chore: atualizar dependências no package-lock.json e ajustar configuração do Vite para simplificar o alias do diretório src

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   ],
   resolve: {
     alias: {
-      "@": new URL("./src", import.meta.url).pathname.replace(/^\//, ""),
+      "@": "/src",
     },
   },
 }));
